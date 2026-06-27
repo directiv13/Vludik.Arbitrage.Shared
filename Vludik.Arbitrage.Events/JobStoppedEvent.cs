90419acc97d3a042483c1e48f285924fd92a3b4b
vlad polieshchuk <>
2026-06-27 04:39:13 +0300

    feat: JobStoppedEvent

---
+++ Vludik.Arbitrage.Events/JobStoppedEvent.cs
@@ -0,0 +1,10 @@
+﻿using Vludik.Arbitrage.Events.Entities;
+
+namespace Vludik.Arbitrage.Events;
+public record JobStoppedEvent(
+    Guid JobId,
+    string Symbol,
+    ExchangeRef BuyExchange,
+    ExchangeRef SellExchange,
+    long Timestamp
+);
